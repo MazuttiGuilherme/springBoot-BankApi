915e87fdfd788916e7ccb6d006f0835aef5ed5a0
MazuttiGuilherme <mazuttimail@gmail.com>
2024-07-06 14:18:55 -0300

    Implementando application-dev.yml

--- src/main/java/projetos/SpringBoot_BankApi/domain/repository/UserRepository.java
+++ src/main/java/projetos/SpringBoot_BankApi/domain/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package projetos.SpringBoot_BankApi.domain.repository;
 
-import org.springframework.data.jpa.reposytory.JpaRepository
 import projetos.SpringBoot_BankApi.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

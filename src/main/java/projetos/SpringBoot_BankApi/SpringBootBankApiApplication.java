21880a6a877b950efc7d49d7e14b037d183671f2
MazuttiGuilherme <mazuttimail@gmail.com>
2024-07-06 12:54:34 -0300

    First Commit

---
+++ src/main/java/projetos/SpringBoot_BankApi/SpringBootBankApiApplication.java
@@ -0,0 +1,13 @@
+package projetos.SpringBoot_BankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projetos/SpringBoot_BankApi/SpringBootBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package projetos.SpringBoot_BankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
